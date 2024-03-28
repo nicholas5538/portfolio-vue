28a460575491c2a5150b5974431a41869f4f15b2
Nicholas <nicholas8399@gmail.com>
2024-03-28 17:35:36 +0800

    Create constants folder to reuse variables or types

---
+++ src/constants/globalVariables.ts
@@ -0,0 +1,15 @@
+export const listElements = [
+  "About Me",
+  "Skills",
+  "Experience",
+  "Projects",
+  "Contact",
+  "Resume",
+];
+
+export const resumeButtonProps = {
+  ariaLabel: "Resume PDF",
+  link: "https://drive.google.com/file/d/1qAhqlPGqBdURD958C8yNdTVteQZKCQBJ/view?usp=sharing",
+  target: "_blank",
+  text: "Resume",
+};
