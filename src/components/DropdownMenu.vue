8f8513870f13ee0eb2f000292baafd2674e854bf
Nicholas <nicholas8399@gmail.com>
2024-04-03 11:15:50 +0800

    Reformat Tailwind CSS syntax with prettier

--- src/components/DropdownMenu.vue
+++ src/components/DropdownMenu.vue
@@ -32,7 +32,7 @@ const lastIndex = listElements.length - 1;
       <DropdownMenuPortal>
         <DropdownMenuContent
           align="end"
-          class="min-w-[300px] max-w-sm z-50 bg-white-400 rounded-tr-xl rounded-br-xl shadow-dropdown will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade dark:bg-black-500 dark:shadow-none"
+          class="z-50 min-w-[300px] max-w-sm rounded-br-xl rounded-tr-xl bg-white-400 shadow-dropdown will-change-[opacity,transform] data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade data-[side=right]:animate-slideLeftAndFade data-[side=top]:animate-slideDownAndFade dark:bg-black-500 dark:shadow-none"
           side="bottom"
           :side-offset="22"
           update-position-strategy="optimized"
@@ -40,7 +40,7 @@ const lastIndex = listElements.length - 1;
           <DropdownMenuItem
             v-for="(list, index) in listElements"
             :key="list"
-            class="cursor-pointer font-mont font-semibold tracking-widest text-black-200 transition duration-200 hover:bg-white-300 hover:text-aqua rounded-none dark:text-white-200 dark:hover:bg-black-400 dark:hover:text-orange"
+            class="cursor-pointer rounded-none font-mont font-semibold tracking-widest text-black-200 transition duration-200 hover:bg-white-300 hover:text-aqua dark:text-white-200 dark:hover:bg-black-400 dark:hover:text-orange"
             :class="{
               'rounded-tr-xl': index === 0,
               'rounded-br-xl': index === lastIndex,
@@ -50,7 +50,7 @@ const lastIndex = listElements.length - 1;
           >
             <NuxtLink
               v-if="index === lastIndex"
-              class="redirect-button block h-full w-full rounded-br-xl rounded-t-none bg-aqua py-2.5 pl-4 font-bold text-white-400 transition-colors duration-300 ease-out hover:bg-dark-aqua hover:no-underline dark:bg-light-orange dark:text-black-300 dark:hover:bg-orange-red"
+              class="redirect-button block h-full w-full rounded-t-none rounded-br-xl bg-aqua py-2.5 pl-4 font-bold text-white-400 transition-colors duration-300 ease-out hover:bg-dark-aqua hover:no-underline dark:bg-light-orange dark:text-black-300 dark:hover:bg-orange-red"
               external
               :aria-label="resumeButtonProps.ariaLabel"
               :target="resumeButtonProps.target"
