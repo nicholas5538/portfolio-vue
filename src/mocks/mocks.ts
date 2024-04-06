1cfd677371a99fcc017db4e18a485cc99f61f451
Nicholas <nicholas8399@gmail.com>
2024-04-06 21:04:54 +0800

    Insert all mock data in a separate folder

---
+++ src/mocks/mocks.ts
@@ -0,0 +1,22 @@
+import type { gitUserSchema } from "~/constants/typeInference";
+import { vi } from "vitest";
+
+const mockUserResponse: Readonly<Partial<gitUserSchema>> = {
+  // eslint-disable-next-line camelcase
+  avatar_url: "",
+  bio: "just a random bio",
+  email: "123@gmail.com",
+  following: 0,
+  followers: 0,
+  location: "Singapore",
+  login: "Nick",
+  name: "Nick Young",
+};
+
+export const mockReturnValues = {
+  data: mockUserResponse,
+  pending: false,
+  error: undefined,
+  refresh: vi.fn(),
+  status: "success",
+};
