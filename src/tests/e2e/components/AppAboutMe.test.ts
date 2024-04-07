4a2a8561bb3833ae6cdea4243e3d0d4eb35fb6e0
Nicholas <nicholas8399@gmail.com>
2024-04-07 23:34:23 +0800

    Create e2e test cases for About Me component

---
+++ src/tests/e2e/components/AppAboutMe.test.ts
@@ -0,0 +1,24 @@
+import { expect, test } from "@nuxt/test-utils/playwright";
+
+test.beforeEach(async ({ page, goto }) => {
+  await goto("/", { waitUntil: "hydration" });
+  await expect(page).toHaveTitle("Nicholas Yong's Portfolio");
+});
+
+test("About Me section heading, link and Lottie component", async ({
+  page,
+}) => {
+  await expect(
+    page.getByLabel("SIT ASE program external link", { exact: true })
+  ).toBeVisible();
+  await expect(
+    page.getByRole("heading", {
+      name:
+        "Since then, I have taken a liking in web development which comprises of\n" +
+        "        problem-solving skills and creativity.",
+    })
+  ).toBeVisible();
+  await expect(
+    page.locator(".lottie-animation-container").first()
+  ).toBeVisible();
+});
