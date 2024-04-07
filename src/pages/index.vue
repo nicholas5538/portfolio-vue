eb8528c060a04746113f3201f1ec076855dcfd1e
Nicholas <nicholas8399@gmail.com>
2024-04-07 19:48:18 +0800

    Create the markup for About Me section
    Might want to try fixing the size of the lottie animation when the theme is switched

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import AppHero from "~/components/AppHero.vue";
+import AppAboutMe from "~/components/AppAboutMe.vue";
 </script>
 
 <template>
   <main>
     <AppHero />
+    <LazyAppAboutMe />
   </main>
 </template>
