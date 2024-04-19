c8d1e432cf3c2e0649c28fb9c8550fc8dac90071
nicholas5538 <nicholas8399@gmail.com>
2024-04-19 15:39:19 +0800

    Exclude constants directory from coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -22,6 +22,7 @@ export default defineVitestConfig({
       "**/.{idea,git,cache,output,temp}/**",
       "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
       "./src/tests/e2e/**",
+      "./src/constants/**",
     ],
     server: {
       deps: {
