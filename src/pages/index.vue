8e891d8900d5f1a427113fcabaa2c6df7da0ea05
Nicholas <nicholas8399@gmail.com>
2024-04-29 20:47:20 +0800

    Remove Lazy Imports for all components as there is no v-if used

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import AppHero from "~/components/AppHero.vue";
+import AppAboutMe from "~/components/AppAboutMe.vue";
+import AppSkills from "~/components/AppSkills.vue";
+import AppWorkExperience from "~/components/AppWorkExperience.vue";
 </script>
 
 <template>
   <main>
     <AppHero />
-    <LazyAppAboutMe />
-    <LazyAppSkills />
-    <LazyAppWorkExperience />
+    <AppAboutMe />
+    <AppSkills />
+    <AppWorkExperience />
   </main>
 </template>
