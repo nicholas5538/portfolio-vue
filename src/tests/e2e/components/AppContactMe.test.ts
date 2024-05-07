91b25fc1505564c2f099d531de0bcc06fbad0940
Nicholas <nicholas8399@gmail.com>
2024-05-07 21:03:29 +0800

    Create E2E test cases for the contact me section

---
+++ src/tests/e2e/components/AppContactMe.test.ts
@@ -0,0 +1,52 @@
+import { expect, test } from "@nuxt/test-utils/playwright";
+
+test.beforeEach(async ({ page, goto }) => {
+  await goto("/", { waitUntil: "hydration" });
+  await expect(page).toHaveTitle("Nicholas Yong's Portfolio");
+});
+
+test.describe("Contact me section testing", () => {
+  test("Contact me heading and button", async ({ page, isMobile }) => {
+    const contactMeHeading = page.getByRole("heading", {
+      name: "Get in touch",
+      exact: true,
+    });
+    const contactMeButton = page.getByLabel(
+      "Click here to send an email to me",
+      {
+        exact: true,
+      }
+    );
+
+    if (!isMobile) {
+      await page
+        .getByRole("listitem")
+        .filter({ hasText: "Contact" })
+        .click({ button: "left" });
+
+      await expect(contactMeHeading).toBeInViewport();
+      await expect(contactMeButton).toBeInViewport();
+      await expect(contactMeButton).toHaveAttribute(
+        "href",
+        "mailto:nicholas8399@gmail.com"
+      );
+    } else {
+      await expect(contactMeHeading).toBeVisible();
+      await expect(contactMeButton).toBeVisible();
+      await expect(contactMeButton).toBeVisible();
+    }
+  });
+
+  test("Dark blue gradient dismount on dark mode", async ({ page }) => {
+    const themeSwitch = page.getByLabel("Appearance", { exact: true });
+    await expect(themeSwitch).toBeInViewport();
+
+    const darkBlueGradient = page.getByTestId("contact-me-dark-blue-gradient");
+    await expect(darkBlueGradient).toBeVisible();
+
+    await themeSwitch.click({ button: "left" });
+    await expect(page.getByTestId("sun")).not.toBeVisible();
+    await expect(page.getByTestId("moon")).toBeInViewport();
+    await expect(darkBlueGradient).not.toBeVisible();
+  });
+});
