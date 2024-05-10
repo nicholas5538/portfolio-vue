9343e253ad7d06636ccde4497f4ea1c8d65ec2d0
Nicholas <nicholas8399@gmail.com>
2024-05-10 21:11:41 +0800

    Implement Container/Presentation pattern for HoverCard component

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -93,93 +93,16 @@ const { data } = await useFetch<gitUserSchema>("/user", {
       </h2>
       <div class="flex flex-row items-center justify-start gap-x-6">
         <div v-for="[key, value] in externalLinks" :key="key">
-          <LazyHoverCard v-if="key === 'GitHub'">
-            <template #cardTrigger>
-              <NuxtLink
-                :aria-label="`Click to see more on my ${key} Profile`"
-                class="focus:shadow-black inline-flex h-[40px] w-[40px] items-center justify-center rounded-full bg-aqua text-white-400 shadow-[0_2px_10px] hover:bg-dark-aqua focus:shadow-[0_0_0_2px] dark:bg-light-orange dark:text-black-300 dark:hover:bg-orange-red"
-                external
-                target="_blank"
-                :to="value.url"
-              >
-                <IconButton :icon="gitHubIcon" />
-              </NuxtLink>
-            </template>
-            <template #cardContent>
-              <div class="flex flex-col gap-[7px]">
-                <AvatarRoot
-                  class="inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full bg-dark-blue align-middle dark:bg-orange-red"
-                >
-                  <AvatarImage as-child :src="data!.avatar_url">
-                    <NuxtImg
-                      class="h-full w-full rounded-[inherit] object-cover"
-                      alt="GitHub Avatar image"
-                      placeholder
-                      format="webp"
-                      loading="lazy"
-                      :src="data?.avatar_url"
-                    />
-                  </AvatarImage>
-                  <AvatarFallback as-child :delay-ms="600">
-                    <NuxtImg
-                      class="h-full w-full rounded-[inherit] object-cover"
-                      alt="Avatar photo"
-                      placeholder
-                      format="webp"
-                      loading="lazy"
-                      src="/images/avatarphoto.jpg"
-                    />
-                  </AvatarFallback>
-                </AvatarRoot>
-                <div
-                  class="flex flex-col gap-4 text-black-100 dark:text-white-100"
-                >
-                  <div class="flex flex-col gap-y-1">
-                    <div class="m-0 text-[15px] font-bold leading-[1.5]">
-                      {{ data?.name }}
-                    </div>
-                    <div class="flex gap-1 font-semibold">
-                      <IconifyIcon :icon="profileIcon" />
-                      <div class="m-0 text-[15px] leading-[1.5]">
-                        {{ data?.login }}
-                      </div>
-                    </div>
-                    <div class="flex gap-1 font-semibold">
-                      <IconifyIcon :icon="mailIcon" />
-                      <div class="m-0 text-[15px] leading-[1.5]">
-                        {{ data?.email }}
-                      </div>
-                    </div>
-                    <div class="flex gap-1 font-semibold">
-                      <IconifyIcon :icon="locationIcon" />
-                      <div class="m-0 text-[15px] leading-[1.5]">
-                        {{ data?.location }}
-                      </div>
-                    </div>
-                  </div>
-                  <div class="m-0 text-[15px] leading-[1.5]">
-                    {{ data?.bio }}
-                  </div>
-                  <div class="flex gap-4">
-                    <div class="flex gap-1">
-                      <div class="m-0 text-[15px] font-medium leading-[1.5]">
-                        {{ data?.following }}
-                      </div>
-                      <div class="m-0 text-[15px] leading-[1.5]">Following</div>
-                    </div>
-                    <div class="flex gap-1">
-                      <div class="m-0 text-[15px] font-medium leading-[1.5]">
-                        {{ data?.followers }}
-                      </div>
-                      <div class="text-mauve10 m-0 text-[15px] leading-[1.5]">
-                        Follower(s)
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </template>
-          </LazyHoverCard>
+          <LazyHoverCard
+            v-if="key === 'GitHub'"
+            :application="key"
+            :application-icon="gitHubIcon"
+            :data="data!"
+            :profile-icon="profileIcon"
+            :mail-icon="mailIcon"
+            :location-icon="locationIcon"
+            :to="value.url"
+          />
           <TooltipProvider v-else :delay-duration="300">
             <Tooltip :icon="value.icon!" :text="value.text" :url="value.url">
               <template #link>
