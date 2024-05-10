2cf27e60918ed8854f9b7dad84dbe08526724632
Nicholas <nicholas8399@gmail.com>
2024-05-10 20:04:15 +0800

    Create a custom icon component
    This cannot be a server component, it is a web component

---
+++ src/components/IconifyIcon.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import "iconify-icon";
+import type { TIcon } from "~/constants/typeInference";
+
+const props = withDefaults(defineProps<TIcon>(), {
+  width: 24,
+  height: 24,
+});
+</script>
+
+<template>
+  <iconify-icon
+    :icon="props.icon"
+    :width="props.width"
+    :height="props.height"
+  />
+</template>
