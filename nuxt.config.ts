91245248d19978183df3cc652aecabdb9e30d4aa
Nicholas <nicholas8399@gmail.com>
2024-05-11 13:48:04 +0800

    Configure PostCSS plugins autoprefixer and cssnano

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -76,7 +76,10 @@ export default defineNuxtConfig({
   postcss: {
     plugins: {
       tailwindcss: {},
-      autoprefixer: {},
+      autoprefixer: { grid: "autoplace" },
+      ...(process.env.NODE_ENV === "production"
+        ? { cssnano: { preset: ["default", { discardUnused: true }] } }
+        : {}),
     },
   },
   runtimeConfig: {
