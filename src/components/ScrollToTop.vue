5e24be65166577280e1824e6817b20c1d10699d7
Nicholas <nicholas8399@gmail.com>
2024-05-17 13:13:46 +0800

    Remove left click from click event as it does not support mobile

--- src/components/ScrollToTop.vue
+++ src/components/ScrollToTop.vue
@@ -14,7 +14,7 @@ function goToTop() {
 </script>
 
 <template>
-  <div v-if="visible" class="relative" @click.left="goToTop">
+  <div v-if="visible" class="relative" @click="goToTop">
     <TooltipProvider :delay-duration="300">
       <Tooltip
         css="tooltip fixed top-[90%] z-50 ml-[85%] cursor-pointer md:ml-[94%] h-[40px] w-[40px]"
