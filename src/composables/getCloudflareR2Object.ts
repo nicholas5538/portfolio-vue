28c214ea84a72974eb39644a24745ef2f0312312
Nicholas <nicholas8399@gmail.com>
2024-06-04 00:41:38 +0800

    Switch to Promise.all implementation
    Improve web performance by using concurrency in fetching presigned URLs from Cloudflare R2

--- src/composables/getCloudflareR2Object.ts
+++ src/composables/getCloudflareR2Object.ts
@@ -1,21 +1,8 @@
-async function useGetCloudflareR2Object(key: string, r2Key: string) {
-  const { data } = await useAsyncData(
-    key,
-    () =>
-      $fetch(`/api/cloudflareR2/${r2Key}`, {
-        method: "get",
-        timeout: 5000,
-      }),
-    {
-      deep: false,
-      getCachedData(key, nuxtApp) {
-        return nuxtApp.payload.data[key] || nuxtApp.static.data[key];
-      },
-      server: true,
-    }
-  );
-
-  return data;
+function useGetCloudflareR2Object(r2Key: string): Promise<string> {
+  return $fetch(`/api/cloudflareR2/${r2Key}`, {
+    method: "get",
+    timeout: 500,
+  });
 }
 
 export default useGetCloudflareR2Object;
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,21 +1,48 @@
 <script setup lang="ts">
 import AppHero from "~/components/AppHero.vue";
 import AppAboutMe from "~/components/AppAboutMe.vue";
+import useGetCloudflareR2Object from "~/composables/getCloudflareR2Object";
+type DataT = {
+  aboutMeLink: string;
+  skillsLink: string;
+  workExperienceLink: string;
+};
 
 definePageMeta({
   layout: "default",
 });
+
+const { data: animationLinks } = await useAsyncData<DataT>(
+  "lottie-json",
+  async () => {
+    const [aboutMeLink, skillsLink, workExperienceLink] = await Promise.all([
+      useGetCloudflareR2Object("animations/aboutme.json"),
+      useGetCloudflareR2Object("animations/skills.json"),
+      useGetCloudflareR2Object("animations/experience.json"),
+    ]);
+
+    return { aboutMeLink, skillsLink, workExperienceLink };
+  },
+  {
+    deep: false,
+    getCachedData(key, nuxtApp) {
+      return nuxtApp.payload.data[key] || nuxtApp.static.data[key];
+    },
+  }
+);
 </script>
 
 <template>
   <main>
     <AppHero />
-    <AppAboutMe />
+    <AppAboutMe :about-me-link="animationLinks?.aboutMeLink" />
     <ClientOnly>
       <LazyScrollToTop />
     </ClientOnly>
-    <LazyAppSkills />
-    <LazyAppWorkExperience />
+    <LazyAppSkills :skills-link="animationLinks?.skillsLink" />
+    <LazyAppWorkExperience
+      :work-experience-link="animationLinks?.workExperienceLink"
+    />
     <LazyAppProjects />
     <LazyAppContactMe />
   </main>
