b248576820230d481ed2f4ef30f2be99334ac306
Nicholas <nicholas8399@gmail.com>
2024-06-17 17:48:02 +0800

    Fix type issues on scrollElementIntoView composable

--- src/composables/scrollElementIntoView.ts
+++ src/composables/scrollElementIntoView.ts
@@ -1,7 +1,7 @@
 import type { TElement } from "~/constants/typeInference";
 
 function useScrollElementIntoView(element: TElement): void {
-  if (element === null) return;
+  if (!element) return;
   return (element as HTMLHeadingElement).scrollIntoView({ behavior: "smooth" });
 }
 
