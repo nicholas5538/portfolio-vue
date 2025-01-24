5ba44f2b61e7b781c362617ae37a4268a286c742
Nicholas <nicholas8399@gmail.com>
2025-01-24 09:24:14 +0800

    Update lint staged configuration

---
+++ .lintstagedrc.js
@@ -0,0 +1,19 @@
+/**
+ * @filename: .lintstagedrc.js
+ * @type {import('lint-staged').Configuration}
+ */
+
+const buildEslintCommand = (filenames) => {
+  if (filenames.length <= 10) {
+    return `eslint --fix ${filenames.join(" ")}`;
+  }
+  return "eslint --fix .";
+};
+
+const buildPrettierWriteCommand = (filenames) =>
+  filenames.map((filename) => `prettier --write ${filename} --ignore-unknown`);
+
+export default {
+  "*.{mjs,js,ts,vue}": [buildPrettierWriteCommand, buildEslintCommand],
+  "*.md": [buildPrettierWriteCommand],
+};
