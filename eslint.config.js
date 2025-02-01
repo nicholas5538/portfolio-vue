d7d2e774e6c74aeeb714d0529db669c0cd0b5af0
Nicholas <nicholas8399@gmail.com>
2025-02-01 18:52:32 +0800

    Fix the eslint config file

--- eslint.config.js
+++ eslint.config.js
@@ -3,36 +3,35 @@ import { createConfigForNuxt } from "@nuxt/eslint-config/flat";
 import eslintConfigPrettier from "eslint-config-prettier";
 import pluginVue from "eslint-plugin-vue";
 
-export default createConfigForNuxt({})
+export default createConfigForNuxt({
+  features: { stylistic: true },
+})
   .append(
-    [
-      ...pluginVue.configs["flat/essential"],
-      {
-        rules: {
-          "vue/multi-word-component-names": "off",
-          "vue/no-unused-vars": "error",
-          "vue/singleline-html-element-content-newline": "off",
-        },
+    ...pluginVue.configs["flat/essential"],
+    {
+      rules: {
+        "vue/multi-word-component-names": "off",
+        "vue/no-unused-vars": "error",
+        "vue/singleline-html-element-content-newline": "off",
       },
-    ],
-    eslintConfigPrettier
+    },
+    eslintConfigPrettier,
+    {
+      ignores: ["src/data/types/", ".husky/install.ts"],
+    }
   )
   .prepend(
-    [
-      {
-        files: ["**/*.js"],
-        rules: js.configs.recommended.rules,
+    {
+      files: ["**/*.js"],
+      rules: js.configs.recommended.rules,
+    },
+    {
+      rules: {
+        "no-unused-vars": "error",
+        "no-undef": "error",
+        "default-case": "error",
+        eqeqeq: "error",
+        quotes: ["error", "double", { avoidEscape: true }],
       },
-    ],
-    [
-      {
-        rules: {
-          "no-unused-vars": "error",
-          "no-undef": "error",
-          "default-case": "error",
-          eqeqeq: "error",
-          quotes: ["error", "double", { avoidEscape: true }],
-        },
-      },
-    ]
+    }
   );
