9e938c5e5e7fc2830c134969d328a35f076f7657
Nicholas <nicholas8399@gmail.com>
2025-03-08 22:13:26 +0800

    Update GitHub GraphQL typings

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,13 @@
 import js from "@eslint/js";
 import { createConfigForNuxt } from "@nuxt/eslint-config/flat";
 import eslintConfigPrettier from "eslint-config-prettier";
-import pluginVue from "eslint-plugin-vue";
 
 export default createConfigForNuxt({
   features: { stylistic: true },
 })
-  .append(
-    ...pluginVue.configs["flat/essential"],
-    {
-      rules: {
-        "vue/multi-word-component-names": "off",
-        "vue/no-unused-vars": "error",
-        "vue/singleline-html-element-content-newline": "off",
-      },
-    },
-    eslintConfigPrettier,
-    {
-      ignores: ["src/data/types/", ".husky/install.ts"],
-    }
-  )
+  .append(eslintConfigPrettier, {
+    ignores: ["src/data/types/", ".husky/install.ts"],
+  })
   .prepend(
     {
       files: ["**/*.js"],
