93163b28fe36a99c8ceb7d6759bc2fd6af8a1fd4
Nicholas <nicholas8399@gmail.com>
2025-08-17 01:24:29 +0800

    Update the hero section

--- app/components/AppHero.vue
+++ app/components/AppHero.vue
@@ -21,94 +21,91 @@ const { data, status } = await getGithubViewer({
 <template>
   <div
     v-once
-    class="mx-auto grid w-full max-w-7xl grid-cols-1 px-4 pt-32 pb-6 md:grid-cols-3 md:grid-rows-3 md:items-center md:justify-between md:gap-x-8 md:pt-60 lg:pb-12 xl:grid-cols-4"
+    class="mx-auto flex w-full max-w-7xl grid-cols-1 flex-col gap-8 px-4 pt-32 pb-6 md:grid md:grid-cols-3 md:items-center md:justify-between md:pt-60 lg:pb-12 xl:grid-cols-4"
   >
-    <div
-      class="group shadow-dropdown z-10 mx-auto mb-8 aspect-2/3 max-h-110 min-h-110 overflow-hidden rounded-3xl md:order-last md:col-span-1 md:row-start-1 md:row-end-4 md:m-0 md:aspect-auto md:max-h-full md:min-h-84 md:w-fit md:justify-self-end lg:min-h-112 xl:col-span-2 xl:min-h-[42.65625rem] dark:shadow-none"
+    <figure
+      class="group shadow-dropdown z-10 mx-auto aspect-2/3 overflow-hidden rounded-3xl md:order-last md:col-span-1 md:aspect-auto md:max-h-full md:w-fit md:justify-self-end xl:col-span-2 dark:shadow-none"
     >
-      <div
+      <NuxtImg
+        alt="My profile picture"
         class="z-20 size-full bg-contain transition-transform duration-300 ease-in-out group-hover:scale-110"
-      >
-        <NuxtImg
-          alt="My profile picture"
-          format="avif,webp"
-          :placeholder="[455, 683, 75, 5]"
-          preload
-          quality="75"
-          sizes="294px md:455px"
-          src="/images/profile-picture.webp"
-        />
-      </div>
-    </div>
-    <h1
-      class="mb-4 md:col-span-2 md:row-start-1 md:row-end-2 md:mb-0 md:text-4xl xl:text-5xl"
-    >
-      <span
-        class="origin-wave animate-wave inline-block align-middle text-4xl xl:text-5xl"
-      >
-        <UiIconifyIcon
-          icon="fluent-emoji:waving-hand"
-          :width="44"
-          :height="44"
-        />
-      </span>
-      Hi there! I&apos;m
-      <span
-        class="from-aqua to-aqua transition-bgi md:text-black-200 dark:bg-orange-gradient bg-transparent bg-linear-to-b bg-clip-text font-bold text-transparent duration-300 ease-out dark:text-transparent"
-      >
-        Nicholas</span
-      >, I love to design and code beautifully simple things.
-    </h1>
-    <h2
-      class="mb-4 md:col-span-2 md:row-start-2 md:row-end-2 md:mb-0 md:text-2xl xl:text-3xl"
-    >
-      Nothing excites me more than learning something new and starting a new
-      adventure.
-    </h2>
-    <section
-      class="space-y-4 md:col-span-2 md:row-start-3 md:row-end-4 md:space-y-8 xl:gap-y-10"
-    >
-      <div class="space-y-2">
-        <h2 class="md:text-2xl xl:text-3xl">
-          Always looking out for opportunities.
-        </h2>
-        <p class="dark:text-white-100 transition-colors duration-300 ease-out">
-          Currently working at
-          <span>
-            <NuxtLink
-              external
-              to="https://bandlabtechnologies.com"
-              rel="noopener noreferrer"
-              target="_blank"
-              >BandLab Technologies</NuxtLink
-            ></span
-          >.
-        </p>
-      </div>
-      <div class="flex w-fit items-center gap-x-4">
-        <template v-for="[key, value] in externalLinks" :key="key">
-          <UiHoverCard
-            v-if="key === 'GitHub' && data?.data.viewer && status === 'success'"
-            :application="key"
-            :application-icon="gitHubIcon"
-            :data="data.data.viewer"
-            :profile-icon="profileIcon"
-            :mail-icon="mailIcon"
-            :location-icon="locationIcon"
+        format="avif,webp"
+        placeholder
+        preload
+        quality="75"
+        sizes="294px md:455px"
+        src="/images/profile-picture.webp"
+      />
+    </figure>
+    <section class="space-y-4 md:col-span-2 md:space-y-8">
+      <h1 class="md:text-4xl xl:text-5xl">
+        <span
+          class="origin-wave animate-wave inline-block align-middle text-4xl xl:text-5xl"
+        >
+          <UiIconifyIcon
+            icon="fluent-emoji:waving-hand"
+            :width="44"
+            :height="44"
           />
-          <TooltipProvider v-else :delay-duration="300">
-            <UiTooltip
-              :icon="value.icon ?? ''"
-              :text="value.text"
-              :url="value.url"
-            >
-              <template #link>
-                <UiIconButton :label="value.text" :icon="value.icon ?? ''" />
-              </template>
-            </UiTooltip>
-          </TooltipProvider>
-        </template>
-      </div>
+        </span>
+        Hi there! I&apos;m
+        <span
+          class="from-aqua to-aqua transition-bgi md:text-black-200 dark:bg-orange-gradient bg-transparent bg-linear-to-b bg-clip-text font-bold text-transparent duration-300 ease-out dark:text-transparent"
+        >
+          Nicholas</span
+        >, I love to design and code beautifully simple things.
+      </h1>
+      <h2 class="md:text-2xl xl:text-3xl">
+        Nothing excites me more than learning something new and starting a new
+        adventure.
+      </h2>
+      <section class="space-y-4 md:col-span-2 md:space-y-8">
+        <hgroup class="space-y-2">
+          <h2 class="md:text-2xl xl:text-3xl">
+            Always looking out for opportunities.
+          </h2>
+          <p
+            class="dark:text-white-100 transition-colors duration-300 ease-out"
+          >
+            Currently working at
+            <span>
+              <NuxtLink
+                external
+                to="https://bandlabtechnologies.com"
+                rel="noopener noreferrer"
+                target="_blank"
+                >BandLab Technologies</NuxtLink
+              ></span
+            >.
+          </p>
+        </hgroup>
+        <nav class="grid w-fit grid-cols-4 gap-x-4">
+          <template v-for="[key, value] in externalLinks" :key="key">
+            <UiHoverCard
+              v-if="
+                key === 'GitHub' && data?.data.viewer && status === 'success'
+              "
+              :application="key"
+              :application-icon="gitHubIcon"
+              :data="data.data.viewer"
+              :profile-icon="profileIcon"
+              :mail-icon="mailIcon"
+              :location-icon="locationIcon"
+            />
+            <TooltipProvider v-else :delay-duration="300">
+              <UiTooltip
+                :icon="value.icon ?? ''"
+                :text="value.text"
+                :url="value.url"
+              >
+                <template #link>
+                  <UiIconButton :label="value.text" :icon="value.icon ?? ''" />
+                </template>
+              </UiTooltip>
+            </TooltipProvider>
+          </template>
+        </nav>
+      </section>
     </section>
   </div>
 </template>
